42a7bb2cc6d2060a5b0d7ea511df0c2127d2e597
Unknown <renske1999@live.nl>
2018-12-05 15:27:03 +0100

    [Added] main

--- app/src/main/java/com/example/sander/locationaware/MainActivity.java
+++ app/src/main/java/com/example/sander/locationaware/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.sander.locationaware;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        ab = new ActionBarDrawerToggle(this, dl, R.string.OpenAB, R.string.CloseAB);
+        ab.setDrawerIndicatorEnabled(true);
+
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.frameId, new MainFragment());
+        tx.commit();
+
+        dl.addDrawerListener(ab);
+        ab.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                item.setChecked(true);
+                dl.closeDrawers();
+                //selectedFragment(item);
+
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dl);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        return ab.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+
+    public boolean selectedFragment(@NonNull MenuItem item){
+        int id = item.getItemId();
+        Fragment fragment = null;
+
+        switch(id){
+            case R.id.MainMenu:
+                fragment = new MainFragment();
+                displaySelectedFragment(fragment);
+                break;
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dl);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void displaySelectedFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameId,fragment);
+        fragmentTransaction.commit();
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
