49badfb1024335b9f46aa2dd50ebe90d46b45f63
Unknown <renske1999@live.nl>
2018-12-05 17:08:23 +0100

    first working map and navigation

--- app/src/main/java/com/example/sander/locationaware/MainFragment.java
+++ app/src/main/java/com/example/sander/locationaware/MainFragment.java
@@ -3,7 +3,9 @@ package com.example.sander.locationaware;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
@@ -15,6 +17,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,82 +26,35 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.w3c.dom.Text;
+
 import java.util.Objects;
 import java.util.zip.Inflater;
 
-public class MainFragment extends Fragment {
+import static android.content.Context.LOCATION_SERVICE;
+import static android.support.v4.content.ContextCompat.getSystemService;
 
-    MapView mMapView;
-    private GoogleMap googleMap;
-    Location location ;
-    LatLng mylocation;
+public class MainFragment extends android.support.v4.app.Fragment  {
+
+    TextView tekst;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_main, container, false);
-        mMapView = (MapView) v.findViewById(R.id.map);
-        mMapView.onCreate(savedInstanceState);
 
-        mMapView.onResume();
+        tekst = v.findViewById(R.id.textView);
 
-        try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                    return;
-                }
-                googleMap.setMyLocationEnabled(true);
-
-               // LatLng sydney = new LatLng(-34, 151);
-                //googleMap.addMarker(new MarkerOptions().position(sydney).title("Merker Title").snippet("Marker Description"));
-                //ameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                //googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-//                CameraUpdate center=CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
-//                CameraUpdate zoom = CameraUpdateFactory.zoomTo(11);
-//                googleMap.moveCamera(center);
-//                googleMap.animateCamera(zoom);
-
-            }
-        });
         return v;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mMapView.onResume();
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mMapView.onPause();
-    }
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mMapView.onLowMemory();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        mMapView.onDestroy();
-    }
 }
--- app/src/main/java/com/example/sander/locationaware/MapsActivity.java
+++ app/src/main/java/com/example/sander/locationaware/MapsActivity.java
@@ -1,18 +1,33 @@
 package com.example.sander.locationaware;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +35,89 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.oldmap);
         mapFragment.getMapAsync(this);
+
+
+
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        ab = new ActionBarDrawerToggle(this, dl, R.string.OpenAB, R.string.CloseAB);
+        ab.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(ab);
+        ab.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final NavigationView nav_view = (NavigationView) findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                item.setChecked(true);
+                dl.closeDrawers();
+                selectedItem(item);
+
+                return true;
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dl);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        return ab.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng kladde = new LatLng(51.564998,   4.266931);
+        googleMap.addMarker(new MarkerOptions().position(kladde).title(" Marker Kladde"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(kladde));
+//        LatLng kladde = new LatLng(51.591431,  4.778150);
+//        googleMap.setMyLocationEnabled(true);
+//        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+          googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kladde, 16));
+//        googleMap.setMinZoomPreference(10);
+//        googleMap.setMaxZoomPreference(22);
+//
+//        UiSettings settings = googleMap.getUiSettings();
+//        settings.setZoomControlsEnabled(true);
+    }
+
+    public boolean selectedItem(@NonNull MenuItem item){
+        int id = item.getItemId();
+        Fragment fragment = null;
+
+        switch(id){
+            case R.id.Maps:
+                Intent intent = new Intent(this, MapsActivity.class);
+                startActivity(intent);
+
+                break;
+            case R.id.info:
+                fragment = new MainFragment();
+                displaySelectedFragment(fragment);
+                break;
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.dl);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void displaySelectedFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameId,fragment);
+        fragmentTransaction.commit();
     }
 }
